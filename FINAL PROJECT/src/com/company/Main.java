af908b2923f08df3dfc30df3b0857cc18c4aa34a
Anna <annikamintel99@yandex.ru>
2021-12-19 00:52:02 +0300

    init: created 2 classes in final project

---
+++ FINAL PROJECT/src/com/company/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Минтель Анна, Группа Пв2-20ПО
+ */
+
+package com.company;
+
+import java.util.Scanner;
+
+public  class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        Money money = new Money();
+        money.setValues(); // устанавливаем значение
+        money.showInfo(); // показываем инф о купюрах и номинале
+        money.countingMoney(); // рассчитываем полную сумму
+        money.enoughMoney(); // достаточно ли денег на покупку товара
+        money.countOfGoods(); // сколько товара можем купить
+
+
+
+        }
+    }
+
+
+
---
+++ FINAL PROJECT/src/com/company/Money.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Money {
+
+        // Переменные
+        int first;//номинал купюры
+        int second; //количество купюр
+        int n; // стоимость единицы товара
+
+        // выводим номинал и количество купюр
+        public void showInfo(){
+            System.out.printf("Номинал купюр: %s$\n Количество купюр: %d\n", first, second);
+        }
+
+        // определяем, хватит ли денежных средств на покупку товара на сумму N рублей
+        public void enoughMoney(){
+            System.out.printf(((first * second / n) >= 1) ?  "\n Денег достаточно" : "\n Денег не достаточно");
+        }
+
+        // определяем, сколько шт товара стоимости n рублей можно купить на имеющиеся денежные средства
+        public void countOfGoods(){
+            int goods = (first * second / n);
+            System.out.printf((goods < 1) ?  "\n Мы не можем купить ни одной единицы товара" :
+                    "\n Мы можем купить: %s единиц товара", goods);
+        }
+
+        // метод, позволяющий получить-установить значение полей
+        public void setValues(){
+            Scanner sc = new Scanner(System.in); // создаем экземпляр класса Scanner
+            System.out.println("Введите номинал купюр:");
+            first = sc.nextInt();
+
+            System.out.println("Введите количество купюр:");
+            second = sc.nextInt();
+
+            System.out.println("Введите стоимость единицы товара:");
+            n = sc.nextInt();
+        }
+
+        // метод, позволяющий рассчитать сумму денег
+        public void countingMoney(){
+            System.out.printf("\n Полная сумма равно: ", first * second);
+        }
+    }
+
+
